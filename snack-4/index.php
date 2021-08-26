1c18bab27beb1fee5823eecc85aee31604dc262d
AlessandroVe <aveneziano536@gmail.com>
2021-08-26 16:01:53 +0200

    .

---
+++ snack-4/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $classe = [
+            [
+                "nome" => "Fabio",
+                "cognome" => "Forghieri",
+                "voti" => [8,5,6,9]
+            ],
+            [
+                "nome" => "Enzo",
+                "cognome" => "Miccio",
+                "voti" => [4,8,10,6]
+            ],
+            [
+                "nome" => "Capitan",
+                "cognome" => "Uncino",
+                "voti" => [10,7,6,3]
+            ]
+        ];
+    ?>
+</body>
+</html>
