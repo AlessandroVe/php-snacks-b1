52132180e01aa55bfe341dcaffc7a71b8a005698
AlessandroVe <aveneziano536@gmail.com>
2021-08-27 11:00:46 +0200

    .

--- snack-5/index.php
+++ snack-5/index.php
@@ -7,8 +7,21 @@
     <title>Document</title>
 </head>
 <body>
+    <h4>Testo non indentato</h4>
     <?php
-        
+        $testo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        echo $testo;
+    
     ?>
+    <hr>
+    <h4>Testo indentato</h4>
+    <div>
+        <?php
+            $testo2= explode(".", $testo);
+            for($i = 0 ; $i< count($testo2)-1; $i++){
+                echo "<p>{$testo2[$i]}</p>";
+            };
+        ?>
+    </div>
 </body>
 </html>
